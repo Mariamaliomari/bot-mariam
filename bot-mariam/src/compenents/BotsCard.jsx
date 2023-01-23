c8b54a40bb3b567734b24befbba681e06662aa5f
Omari99 <mariam.ali01@student.moringaschool.com>
2023-01-23 22:19:30 +0300

    add db.json file

---
+++ bot-mariam/src/compenents/BotsCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const botTypeClasses = {
+    Assault: "icon military",
+    Defender: "icon shield",
+    Support: "icon plus circle",
+    Medic: "icon ambulance",
+    Witch: "icon magic",
+    Captain: "icon star",
+  };
